bdcabc4b3febc0a42f2ff9ad677a74a4cd0e8ec5
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-03-18 22:29:59 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,31 @@ class ProjectInput {
     templateElement : HTMLTemplateElement;
     hostElement : HTMLDivElement;
     element : HTMLFormElement;
+    titelInputElement : HTMLInputElement;
+    descriptionlInputElement : HTMLInputElement;
+    peopleInputElement : HTMLInputElement;
     constructor() {
         // Access to the elements inside the DOM
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
         const importedNode = document.importNode(this.templateElement.content,true);
         this.element = importedNode.firstElementChild as HTMLFormElement ;
+        this.element.id = 'user-input';
+        this.titelInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionlInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.configure();
         this.renderElement()
+
+    }
+    private sumbitHandler (event : Event) 
+    {
+        // the default behavior is to send an http request
+        event.preventDefault();
+    }
+
+    private configure () {
+        this.element.addEventListener('submit', this.sumbitHandler.bind(this));
     }
     private renderElement() {
         // method that insert's an html element 

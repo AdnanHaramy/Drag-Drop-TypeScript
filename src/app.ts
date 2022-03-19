52c6309e4866b52c6e75a41d89edd37d062a3cc7
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2022-03-19 14:21:38 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// Reusable Validation Logic 
+// required and minLength and maxLength and min and max should be optional
+interface Validatable {
+    value : string | number;
+    required? : boolean ;
+    minLength? : number ;
+    maxLength? : number;
+    min? : number;
+    max?: number; 
+}
+// gets an object which has the validatable structure 
+function validate(validatableInput : Validatable) {
+    let isValid = true;
+    if(validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0 ;
+    }
+    // the minLength is set and its of type string 
+    if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if(validatableInput.maxLength != null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid =isValid && validatableInput.value >= validatableInput.min ;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid =isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+
 class ProjectInput {
     // properties and thier types
     templateElement : HTMLTemplateElement;
@@ -25,9 +57,29 @@ class ProjectInput {
         const enteredTitle = this.titelInputElement.value;
         const enteredDescription = this.descriptionlInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
-        if(enteredTitle.trim().length === 0 || 
-        enteredDescription.trim().length === 0 || 
-        enteredPeople.trim().length ===0 ) {
+
+        const titleValidatable : Validatable = {
+            value : enteredTitle ,
+            required :true,
+        };
+        const descriptionValidatable : Validatable = {
+            value : enteredDescription ,
+            required :true,
+            minLength : 5
+        };
+        const peopleValidatable : Validatable = {
+            value : +enteredPeople ,
+            required :true,
+            min :1,
+            max : 5
+        };
+
+
+        if(
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable) 
+        ) {
             alert('invalid input please try again ')
             return;
         }
